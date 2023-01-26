e4111b413872f97c4882dd23a865d4074fd81120
Radek Szynwelski <121770151+radekszynwelski97@users.noreply.github.com>
2023-01-26 10:55:26 +0100

    Dodanie struktur sterujących switch case

---
+++ Specyfikatory i pakiety/src/Logic/PointController.java
@@ -0,0 +1,23 @@
+package Logic;
+import Data.Point;
+
+public class PointController {
+
+    public void addX(Point p){
+        p.setX(p.getX() + 1 );
+    }
+
+    public  void minusX(Point p ){
+        p.setX(p.getX() - 1 );
+    }
+
+    public void addY(Point p ){
+        p.setY(p.getY() + 1 );
+    }
+
+    public void minusY(Point p ){
+        p.setY(p.getY() - 1 );
+    }
+
+
+}
